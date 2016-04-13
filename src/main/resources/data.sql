e6cb90cedace0629486d6ad6e0d7a45486d411b9
iwamin <author@example.com>
2016-04-13 18:50:50 +0900

    サンプルコード削除

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,1 @@
-INSERT INTO customers(first_name, last_name) VALUES('Nobita', 'Nobi');
-INSERT INTO customers(first_name, last_name) VALUES('Takeshi', 'Goda');
-INSERT INTO customers(first_name, last_name) VALUES('Suneo', 'Honekawa');
-INSERT INTO customers(first_name, last_name) VALUES('Shizuka', 'Minamoto');
-
 insert into users(user_id, password) values ('hoge', 'hoge')
